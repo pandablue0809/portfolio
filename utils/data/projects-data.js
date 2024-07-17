ea9962f4ca25cc02ecb5485b5ec4c932872ec0fe
pandablue0809 <bluep0415@gmail.com>
2024-07-17 18:18:21 +0900

    fix project7~9

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -2,9 +2,12 @@ import shop from '/public/image/123.png';
 import crefin from '/public/image/landing-animation.gif';
 import realEstate from '/public/image/real-estate.png';
 import flashloan from '/public/image/flashloan.png';
-import bc1 from '/public/image/bc1.png';
+import allinone from '/public/image/allinone.png';
 import bc2 from '/public/image/bc2.png';
 import travel from '/public/image/travel-app.gif';
+import clothes from '/public/image/clothes.png';
+import gamadant from '/public/image/gamadant.png';
+
 
 export const projectsData = [
     {
@@ -40,30 +43,20 @@ export const projectsData = [
         image: shop,
         tags:['react', 'nodejs', 'ecommerce', 'mongodb',  'exressjs']
     },    
+            
     {
         id: 4,
-        name: 'Travel App',
-        description: 'A travel app that obtains a desired trip location & date from the user, and displays the predicted weather forecast and an image of the location using information obtained from external APIs',
-        tools: ['nodejs', 'babel' ,  'weather-forecast' ],
-        role: 'Fullstack developer',
-        code: 'https://github.com/pandablue0809/travel-app',
-        demo: 'https://gotaplan.onrender.com/',
-        image: travel,
-        tags:['Javascript','nodejs', 'babel' , 'weather-forecast' ]
-    },    
-    {
-        id: 5,
-        name: 'Sqf coin ERC20 Smart Contract',
+        name: 'Smart Contract',
         description: "My team and I developed a Sqf coin ERC20 Smart Contract",
         tools: ['React', 'Material UI', 'Web3.js', 'Solidity' ],
         code: 'https://github.com/pandablue0809/pillar-',
-        demo: 'https://pillar-landing-app.vercel.app/',
+        demo: 'https://sqfcoin.com/',
         image: bc2,
         role: 'Full stack developer',
-        tags:[]
+        tags:['Solidity','Web3.js','React.js']
     },
     {
-        id: 6,
+        id: 5,
         name: 'flash-loan-bot',
         description: "Crypto arbitrage bot that searches for a significant difference in crypto prices on a number of exchanges and will execute the simultaneous buying and selling of said crypto through a flash loan provided by the AAVE Flash Loan Smart Contract V2.",
         tools: ['nodejs', 'javascript', 'ethereum', 'dapp', 'cryptocurrency', 'solidity'],
@@ -72,40 +65,52 @@ export const projectsData = [
         image: flashloan,
         role: 'Blockchain Developer',
         tags:['solidity','cryptocurrency','nodejs', 'ethereum', 'dapp']
-    },    
+    },  
+    {
+        id: 6,
+        name: 'All-in-one platform ',
+        description: 'I have designed and developed a All-in-one platform for decentralized asset trading and arbitrage. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick.',
+        tools: ['NextJS', "Web3.js", "NestJS", "TypeScript"],
+        role: 'Web3 Developer',
+        code: 'https://github.com/pandablue0809/',
+        demo: 'https://uruloki.vercel.app/',
+        image: allinone,
+        tags:['NextJS', "Web3.js", "NestJS", "TypeScript"]
+    },
     {
         id: 7,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
+        name: 'Travel App',
+        description: 'A travel app that obtains a desired trip location & date from the user, and displays the predicted weather forecast and an image of the location using information obtained from external APIs',
+        tools: ['nodejs', 'babel' ,  'weather-forecast' ],
+        role: 'Fullstack developer',
+        code: 'https://github.com/pandablue0809/travel-app',
+        demo: 'https://gotaplan.onrender.com/',
+        image: travel,
+        tags:['Javascript','nodejs', 'babel' , 'weather-forecast' ]
+    },  
+    {
+        id: 8,
+        name: 'Clothes Shop',
+        description: 'Participated in building and deploying a new eCommerce platform that integrates different systems, APIs and many others to support the logic of processing customer orders',
         tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
+        code: 'https://github.com/pandablue0809/',
         role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-        tags:['1','2','3']
+        demo: 'https://www.jackthreads.com/',
+        image: clothes,
+        tags:['React','TypeScript', 'MongoDB', 'Azure Blob','Stripe']
     },
     {
-        id: 8,
-        name: 'All-in-one platform for decentralized asset trading and arbitrage',
-        description: 'I have designed and developed a All-in-one platform for decentralized asset trading and arbitrage. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Web3.js, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', "Web3.js", "NestJS", "TypeScript"],
-        role: 'Blockchain Developer',
-        code: '',
-        demo: '',
-        image: bc1,
-        tags:[]
+        id: 9,
+        name: 'Gamedent',
+        description: 'I have designed and developed a clinic service. I designed and implemented RESTful APIs, enabling seamless data exchange between the frontend and backend components',
+        tools: ['NextJS', 'Tailwind CSS', "TypeScript"],
+        role: 'Fullstack Developer',
+        code: 'https://github.com/pandablue0809/',
+        demo: 'https://gama-dental.vercel.app/',
+        image: gamadant,
+        tags:['NextJS', 'Tailwind CSS', "TypeScript"]
     },
-    {
-        id: 8,
-        name: 'All-in-one platform for decentralized asset trading and arbitrage',
-        description: 'I have designed and developed a All-in-one platform for decentralized asset trading and arbitrage. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Web3.js, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', "Web3.js", "NestJS", "TypeScript"],
-        role: 'Blockchain Developer',
-        code: '',
-        demo: '',
-        image: bc1,
-        tags:[]
-    },
+    
 ];
 
 
