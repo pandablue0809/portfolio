2373de0fb3b750276fc57b1e7c9718b7ae1e051f
pandablue0809 <bluep0415@gmail.com>
2024-07-17 17:06:27 +0900

    fix project2~5

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,9 +1,10 @@
-import ayla from '/public/image/ayla.jpg';
+import shop from '/public/image/123.png';
 import crefin from '/public/image/landing-animation.gif';
-import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
+import realEstate from '/public/image/real-estate.png';
+import flashloan from '/public/image/flashloan.png';
 import bc1 from '/public/image/bc1.png';
 import bc2 from '/public/image/bc2.png';
+import travel from '/public/image/travel-app.gif';
 
 export const projectsData = [
     {
@@ -16,61 +17,61 @@ export const projectsData = [
         demo: 'https://dynamitetrade.vercel.app/',
         image: crefin,
         tags:['next.js','chatgpt-api','prisma','shadcn-ui']
-    },
+    },        
     {
         id: 2,
+        name: 'Real Estate',
+        description: 'Pillar is a commercial real estate investment platform designed to facilitate discovery, diligence, execution, and ongoing management of direct deals for institutional CRE investors.',
+        tools: ['React', 'SCSS', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+        code: 'https://github.com/pandablue0809/AI-Powered-Financial-App',
+        role: 'Full Stack Developer',
+        demo: 'https://dynamitetrade.vercel.app/',
+        image: realEstate,
+        tags:['React', 'SCSS', 'Express', 'TypeScript']
+    },
+    {
+        id: 3,
+        name: 'E-Commerce',
+        description: "MERN-E-Commerce-Frontend is the frontend of an e-commerce web application built with ReactJS ,Material UI, ContextAPI, React-router-dom. MERN-E-Commerce-Backend is the backend of an e-commerce web application built with MongoDB, NodeJS, and ExpressJS.",
+        tools: ['react', 'nodejs', 'ecommerce', 'mongodb',  'exressjs'],
+        role: 'Backend Developer',
+        code: 'https://github.com/pandablue0809/Ecommerce_frontend_MERN',
+        demo: 'https://e-shopit.vercel.app/',
+        image: shop,
+        tags:['react', 'nodejs', 'ecommerce', 'mongodb',  'exressjs']
+    },    
+    {
+        id: 4,
+        name: 'Travel App',
+        description: 'A travel app that obtains a desired trip location & date from the user, and displays the predicted weather forecast and an image of the location using information obtained from external APIs',
+        tools: ['nodejs', 'babel' ,  'weather-forecast' ],
+        role: 'Fullstack developer',
+        code: 'https://github.com/pandablue0809/travel-app',
+        demo: 'https://gotaplan.onrender.com/',
+        image: travel,
+        tags:['Javascript','nodejs', 'babel' , 'weather-forecast' ]
+    },    
+    {
+        id: 5,
         name: 'Sqf coin ERC20 Smart Contract',
         description: "My team and I developed a Sqf coin ERC20 Smart Contract",
         tools: ['React', 'Material UI', 'Web3.js', 'Solidity' ],
-        code: '',
-        demo: '',
+        code: 'https://github.com/pandablue0809/pillar-',
+        demo: 'https://pillar-landing-app.vercel.app/',
         image: bc2,
-        role: 'Blockchain Developer',
-        tags:[]
-    },    
-    {
-        id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-        tags:['1','2','3']
-    },
-    {
-        id: 4,
-        name: 'All-in-one platform for decentralized asset trading and arbitrage',
-        description: 'I have designed and developed a All-in-one platform for decentralized asset trading and arbitrage. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Web3.js, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', "Web3.js", "NestJS", "TypeScript"],
-        role: 'Blockchain Developer',
-        code: '',
-        demo: '',
-        image: bc1,
-        tags:[]
-    },
-    {
-        id: 5,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
-        code: '',
-        demo: '',
-        image: crefin,
+        role: 'Full stack developer',
         tags:[]
     },
     {
         id: 6,
-        name: 'Sqf coin ERC20 Smart Contract',
-        description: "My team and I developed a Sqf coin ERC20 Smart Contract",
-        tools: ['React', 'Material UI', 'Web3.js', 'Solidity' ],
-        code: '',
-        demo: '',
-        image: bc2,
+        name: 'flash-loan-bot',
+        description: "Crypto arbitrage bot that searches for a significant difference in crypto prices on a number of exchanges and will execute the simultaneous buying and selling of said crypto through a flash loan provided by the AAVE Flash Loan Smart Contract V2.",
+        tools: ['nodejs', 'javascript', 'ethereum', 'dapp', 'cryptocurrency', 'solidity'],
+        code: 'https://github.com/pandablue0809/flash-bot-solidity',
+        demo: 'https://github.com/pandablue0809/flash-bot-solidity',
+        image: flashloan,
         role: 'Blockchain Developer',
-        tags:[]
+        tags:['solidity','cryptocurrency','nodejs', 'ethereum', 'dapp']
     },    
     {
         id: 7,
